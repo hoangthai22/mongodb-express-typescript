1503cb54256d97e05b877eb7ca9201bd2c944f2e
hoangthai22 <hoangthai22tv@gmail.com>
2023-02-11 13:03:11 +0700

    update

--- src/routes/category.route.ts
+++ src/routes/category.route.ts
@@ -6,16 +6,19 @@ const categoryRoute = () => {
     const storage = multer.memoryStorage();
     const upload = multer({ storage });
     const router = Router();
-
-    router.post('/category', upload.single('image'), createCategory);
+    router.route('/category')
+        .all(verifyToken)
+        .post(upload.single('image'), createCategory);
 
     router.get('/categories', verifyToken, getAllCategories);
 
-    router.get('/category/:id', getCategory);
+    router.get('/category/:id', verifyToken, getCategory);
 
-    router.put('/category/:id', upload.single('image'), updateCategory);
+    router.route('/category/:id')
+        .all(verifyToken)
+        .put(upload.single('image'), updateCategory);
 
-    router.delete('/category/:id', deleteCategory);
+    router.delete('/category/:id', verifyToken, deleteCategory);
 
     return router;
 };
--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { createProduct, deleteProduct, getAllProducts, getProduct, updateProduct } from '../controllers/product.controller';
 import multer from 'multer';
+import { verifyToken } from '../middlewares/authJwt';
 
 const productRoute = () => {
   const router = Router();
@@ -8,13 +9,13 @@ const productRoute = () => {
   const upload = multer({ storage });
   router.post('/product', upload.single('image'), createProduct);
 
-  router.get('/products', getAllProducts);
+  router.get('/products', verifyToken, getAllProducts);
 
-  router.get('/product/:id', getProduct);
+  router.get('/product/:id', verifyToken, getProduct);
 
   router.put('/product/:id', upload.single('image'), updateProduct);
 
-  router.delete('/product/:id', deleteProduct);
+  router.delete('/product/:id', verifyToken, deleteProduct);
 
   return router;
 };
